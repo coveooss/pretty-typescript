401c84590e88deef5730bca4a6704641975cca20
Francois LG <francois.lguillemette@gmail.com>
2017-04-11 16:42:43 -0400

    Allows to use the file from the working directory if it exists Fix tsfmtOptions not used anywhere

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const lazypipe = require('lazypipe');
 const tslint = require('gulp-tslint');
 const tsfmt = require('./gulp-typescript-formatter');
+const fs = require("fs");
+const process = require("process");
+const path = require("path");
 
-const tslintOptions = require('./tslint.json');
-const tsfmtOptions = require('./tsfmt.json');
+const currentWorkingDirectory = process.cwd();
+
+const tslintOptions = loadRequiredOptions('tslint.json');
+const tsfmtOptions = loadRequiredOptions('tsfmt.json');
 
 module.exports = (options) => {
   return lazypipe()
-  .pipe(tsfmt, { baseDir: __dirname, tsfmt: true, tslint: true })
+  .pipe(tsfmt, Object.assign({ baseDir: __dirname }, tsfmtOptions))
   .pipe(tslint, { configuration: tslintOptions, formatter: 'prose' })
   .pipe(tslint.report, Object.assign({ emitError: false }, options))();
+}
+
+function loadRequiredOptions(fileName) {
+  let fileInWorkingDirectory = path.join(currentWorkingDirectory, fileName);
+  if (fs.existsSync(fileInWorkingDirectory)) {
+    return require(fileInWorkingDirectory);
+  } else {
+    return require(`./${fileName}`)
+  }
 }

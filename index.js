771d0be4fcf9c3f1ae6b76fa7af0fb1d23b8ffa9
Francois LG <francois.lguillemette@gmail.com>
2017-04-12 08:38:09 -0400

    Correctly change base directory if tsfmt.json is in the current directory Also, throw if you have only one of the files since you need both.

--- index.js
+++ index.js
@@ -5,23 +5,24 @@ const fs = require("fs");
 const process = require("process");
 const path = require("path");
 
-const currentWorkingDirectory = process.cwd();
+const cwd = process.cwd();
 
-const tslintOptions = loadRequiredOptions('tslint.json');
-const tsfmtOptions = loadRequiredOptions('tsfmt.json');
+const pathToTsfmtInWorkingDirectory = path.join(cwd, "tsfmt.json");
+const hasTsfmtInWorkingDirectory = fs.existsSync(pathToTsfmtInWorkingDirectory);
+
+const pathToTslintInWorkingDirectory = path.join(cwd, "tslint.json");
+const hasTslintInWorkingDirectory = fs.existsSync(pathToTslintInWorkingDirectory);
+
+if (hasTsfmtInWorkingDirectory !== hasTslintInWorkingDirectory) {
+  throw "You must have both a 'tslint.json' and a 'tsfmt.json' file when using the working directory configuration.";
+}
+
+const tsfmtPath = hasTsfmtInWorkingDirectory ? cwd : __dirname;
+const tslintOptions = hasTslintInWorkingDirectory ? require(pathToTslintInWorkingDirectory) : require("./tslint.json")
 
 module.exports = (options) => {
   return lazypipe()
-  .pipe(tsfmt, Object.assign({ baseDir: __dirname }, tsfmtOptions))
+  .pipe(tsfmt, Object.assign({ baseDir: tsfmtPath, tsfmt: true, tslint: true }))
   .pipe(tslint, { configuration: tslintOptions, formatter: 'prose' })
   .pipe(tslint.report, Object.assign({ emitError: false }, options))();
-}
-
-function loadRequiredOptions(fileName) {
-  let fileInWorkingDirectory = path.join(currentWorkingDirectory, fileName);
-  if (fs.existsSync(fileInWorkingDirectory)) {
-    return require(fileInWorkingDirectory);
-  } else {
-    return require(`./${fileName}`)
-  }
 }
